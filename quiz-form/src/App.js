eae1f959637157c42d20da350f969c2a56e234f3
Abdul Moiz Siraj <abdulmoiz68@gmail.com>
2020-11-14 19:28:51 +0500

    header done

---
+++ quiz-form/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/Header';
+import InfoCard from './components/InfoCard';
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ quiz-form/src/components/InfoCard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+const InfoCard = () => {
+  return (
+    <div>
+      <Container fluid>
+        <Row>
+          <Col style={{ paddingLeft: '10vh', paddingRight: '10vh' }}>
+            <Card className='shadow'>
+              <Card.Body>
+                <Card.Title>
+                  <strong>Questions will be typed here</strong>
+                </Card.Title>
+                <Card.Subtitle className='mb-2 text-muted'>
+                  Correct answers will be here
+                </Card.Subtitle>
+              </Card.Body>
+            </Card>{' '}
+          </Col>{' '}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default InfoCard;
